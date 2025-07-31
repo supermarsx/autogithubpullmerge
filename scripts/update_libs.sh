515c3f8b6a467a4204a62cda6a48f30862af0e99
Mariana M <github_618a@imariana.com>
2025-07-31 12:33:19 +0100

    fix: clone missing dependencies

--- scripts/update_libs.sh
+++ scripts/update_libs.sh
@@ -5,17 +5,19 @@ LIBS_DIR="${SCRIPT_DIR}/../libs"
 mkdir -p "$LIBS_DIR"
 
 clone_or_update() {
-  local repo="$1"
-  local dir="$LIBS_DIR/$2"
-  if [ -d "$dir/.git" ]; then
-    git -C "$dir" pull --ff-only
-  else
-    git clone --depth 1 "$repo" "$dir"
-  fi
+	local repo="$1"
+	local dir="$LIBS_DIR/$2"
+	if [ -d "$dir/.git" ]; then
+		git -C "$dir" pull --ff-only
+	else
+		git clone --depth 1 "$repo" "$dir"
+	fi
 }
 
 clone_or_update https://github.com/CLIUtils/CLI11.git CLI11
 clone_or_update https://github.com/jbeder/yaml-cpp.git yaml-cpp
+clone_or_update https://github.com/yaml/libyaml.git libyaml
 clone_or_update https://github.com/nlohmann/json.git json
 clone_or_update https://github.com/gabime/spdlog.git spdlog
-
+clone_or_update https://github.com/curl/curl.git curl
+clone_or_update https://github.com/sqlite/sqlite.git sqlite
