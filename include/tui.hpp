6f292b3697b2528b42766339558061a8f343f0aa
Mariana M <github_618a@imariana.com>
2025-07-31 21:53:59 +0100

    fix: robust include detection

--- include/tui.hpp
+++ include/tui.hpp
@@ -1,7 +1,17 @@
 #ifndef AUTOGITHUBPULLMERGE_TUI_HPP
 #define AUTOGITHUBPULLMERGE_TUI_HPP
 
+#if __has_include(<curses.h>)
 #include <curses.h>
+#elif __has_include(<ncurses.h>)
+#include <ncurses.h>
+#elif __has_include(<ncurses/curses.h>)
+#include <ncurses/curses.h>
+#elif __has_include("../libs/ncurses/include/curses.h")
+#include "../libs/ncurses/include/curses.h"
+#else
+#error "curses.h not found"
+#endif
 
 namespace agpm {
 
