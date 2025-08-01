b3d9cab59236a20a93155012619ecc2132c3630a
Mariana M <github_618a@imariana.com>
2025-08-01 13:43:23 +0100

    feat(cli): support include/exclude repo lists

--- src/app.cpp
+++ src/app.cpp
@@ -10,6 +10,8 @@ namespace agpm {
 
 int App::run(int argc, char **argv) {
   options_ = parse_cli(argc, argv);
+  include_repos_ = options_.include_repos;
+  exclude_repos_ = options_.exclude_repos;
   spdlog::level::level_enum lvl =
       options_.verbose ? spdlog::level::debug : spdlog::level::info;
   try {
