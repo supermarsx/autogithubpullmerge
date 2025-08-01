ee08bebedbaf69d2466d691b438862547b3fed8a
Mariana M <github_618a@imariana.com>
2025-08-01 17:33:02 +0100

    Revert "Fix Windows compile paths"

--- scripts/compile_win.ps1
+++ scripts/compile_win.ps1
@@ -1,35 +1,18 @@
 $ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$RootDir   = Resolve-Path (Join-Path $ScriptDir '..')
+$RootDir = Resolve-Path (Join-Path $ScriptDir '..')
+Set-Location $RootDir
 
-$BuildDir  = Join-Path $RootDir 'build_gpp'
-New-Item -ItemType Directory -Path $BuildDir -Force | Out-Null | Out-Null
+$BuildDir = Join-Path $RootDir 'build_gpp'
+New-Item -ItemType Directory -Path $BuildDir -Force | Out-Null
 
-$srcFiles   = Get-ChildItem -Path (Join-Path $RootDir 'src') -Filter *.cpp -Recurse | ForEach-Object { $_.FullName }
-$includeDir = Join-Path $RootDir 'include'
-$libsDir    = Join-Path $RootDir 'libs'
+$srcFiles = Get-ChildItem -Path (Join-Path $RootDir 'src') -Filter *.cpp -Recurse | ForEach-Object { $_.FullName }
+$srcList = $srcFiles -join ' '
 
-$includeArgs = @(
-    "-I$includeDir",
-    "-I$libsDir\CLI11\include",
-    "-I$libsDir\json\include",
-    "-I$libsDir\spdlog\include",
-    "-I$libsDir\yaml-cpp\include",
-    "-I$libsDir\libyaml\include",
-    "-I$libsDir\pdcurses",
-    "-I$libsDir\curl\include",
-    "-I$libsDir\sqlite"
-) -join ' '
+$include = Join-Path $RootDir 'include'
+$libsDir  = Join-Path $RootDir 'libs'
+$includeArgs = "-I`"$include`" -I`"$libsDir\CLI11\include`" -I`"$libsDir\json\include`" -I`"$libsDir\spdlog\include`" -I`"$libsDir\yaml-cpp\include`" -I`"$libsDir\libyaml\include`" -I`"$libsDir\pdcurses`" -I`"$libsDir\curl\include`" -I`"$libsDir\sqlite`""
 
-$libArgs = @(
-    "-L$libsDir\curl\lib",
-    "-L$libsDir\sqlite",
-    "-L$libsDir\yaml-cpp\lib",
-    "-L$libsDir\libyaml\lib",
-    "-L$libsDir\pdcurses\lib"
-) -join ' '
+$libArgs = "-L`"$libsDir\curl\lib`" -L`"$libsDir\sqlite`" -L`"$libsDir\yaml-cpp\lib`" -L`"$libsDir\libyaml\lib`" -L`"$libsDir\pdcurses\lib`""
 
-$cmd = "g++ -std=c++20 -Wall -Wextra -O2 -static -static-libgcc -static-libstdc++ -DYAML_CPP_STATIC_DEFINE $includeArgs $($srcFiles -join ' ') $libArgs -lcurl -lsqlite3 -lyaml-cpp -lyaml -lpdcurses -o `"$BuildDir\autogithubpullmerge.exe`""
-
-Push-Location $RootDir
+$cmd = "g++ -std=c++20 -Wall -Wextra -O2 -static -static-libgcc -static-libstdc++ -DYAML_CPP_STATIC_DEFINE $includeArgs $srcList $libArgs -lcurl -lsqlite3 -lyaml-cpp -lyaml -lpdcurses -o `"$BuildDir\autogithubpullmerge.exe`""
 Invoke-Expression $cmd
-Pop-Location
