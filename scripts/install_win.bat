07b3eb452bff172bb3e0074663d11b01a00283da
Mariana <github_618a@imariana.com>
2025-08-12 17:18:39 +0100

    build: integrate cross-platform dependency fixes

--- scripts/install_win.bat
+++ scripts/install_win.bat
@@ -1,2 +1,14 @@
 @echo off
+setlocal
+
 choco install cmake git curl sqlite spdlog mingw -y
+
+if "%VCPKG_ROOT%"=="" (
+    git clone https://github.com/microsoft/vcpkg "%~dp0..\vcpkg" || exit /b 1
+    set "VCPKG_ROOT=%~dp0..\vcpkg"
+    call "%VCPKG_ROOT%\bootstrap-vcpkg.bat" || exit /b 1
+)
+
+"%VCPKG_ROOT%\vcpkg.exe" install libev c-ares zlib brotli openssl ngtcp2 nghttp3 jansson libevent libxml2 jemalloc || exit /b 1
+
+endlocal
