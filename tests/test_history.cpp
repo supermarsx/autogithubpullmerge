6c3f723bce29ef645213d44193f6d9cae649ee35
Mariana <github_618a@imariana.com>
2025-08-25 19:28:30 +0100

    fix GitHubPoller test arguments

--- tests/test_history.cpp
+++ tests/test_history.cpp
@@ -43,8 +43,8 @@ TEST_CASE("test history") {
   char *argv[] = {prog, csv_flag, csv_path, json_flag, json_path};
   CliOptions opts = parse_cli(5, argv);
 
-  GitHubPoller poller(client, {{"me", "repo"}}, 0, 120, false, false, false, "",
-                      false, false, "", &hist);
+  GitHubPoller poller(client, {{"me", "repo"}}, 0, 120, 1, false, false, false,
+                      "", false, false, "", &hist);
 
   if (!opts.export_csv.empty() || !opts.export_json.empty()) {
     poller.set_export_callback([&]() {
